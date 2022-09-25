a13408a84ff4f0a4d9ce2da92c50121197f2f2c2
Nurbolot <nurbolot.boobekovv@gmail.com>
2022-09-25 13:06:27 +0300

    fixed bugs

--- db.js
+++ db.js
@@ -1,9 +1,6 @@
 const Pool = require("pg").Pool;
-const dotenv = require("dotenv");
 
-dotenv.config();
-
-const connectionString = process.env.DB_CONNECTION;
+const connectionString = `postgres://nurbolot:nOJpQy5zuU0eWBZRRCtgqZD7iwOLTw5S@dpg-ccnuq1ta49940mp3jt5g-a.oregon-postgres.render.com/userdatabse1?ssl=true`;
 
 const db = new Pool({
   connectionString,
